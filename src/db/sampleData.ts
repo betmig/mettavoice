52533cdd8202786b16ffcd3bba83f268b933ab6b
Betmig Link <hola@betmig.link>
2024-11-27 22:35:10 +0100

    Adding multiple Mettā Suttas

--- src/db/sampleData.ts
+++ src/db/sampleData.ts
@@ -2,18 +2,23 @@ export const sampleSuttas = [
   {
     title: "Goodwill / Mettā Sutta",
     content: `“Monks, for one whose awareness-release through goodwill is cultivated, developed, pursued, given a means of transport, given a grounding, steadied, consolidated, and well-undertaken, eleven benefits can be expected. Which eleven?
-              “One sleeps easily, wakes easily, dreams no evil dreams. One is dear to human beings, dear to non-human beings. The deevas protect one. Neither fire, poison, nor weapons can touch one. One’s mind gains concentration quickly. One’s complexion is bright. One dies unconfused and, if penetrating no higher, is headed for a Brahmā world.
+              “One sleeps easily, wakes easily, dreams no evil dreams. One is dear to human beings, dear to non-human beings. The devas protect one. Neither fire, poison, nor weapons can touch one. One’s mind gains concentration quickly. One’s complexion is bright. One dies unconfused and, if penetrating no higher, is headed for a Brahmā world.
               “These are the eleven benefits that can be expected for one whose awareness-release through goodwill is cultivated, developed, pursued, handed the reins, taken as a basis, steadied, consolidated, and well-undertaken.”`,
     source: "AN 11:16",
     sourceUrl: "https://www.dhammatalks.org/suttas/AN/AN11_16.html",
     category: "Goodwill",
   },
   {
-    title: "The Noble Search",
-    content: `"Monks, there are these two searches: ignoble search & noble search. And what is ignoble search? There is the case where a person, being subject himself to birth, seeks (happiness in) what is likewise subject to birth. Being subject himself to aging… illness… death… sorrow… defilement, he seeks (happiness in) what is likewise subject to illness… death… sorrow… defilement."`,
-    source: "MN 26",
-    sourceUrl: "https://www.dhammatalks.org/suttas/MN/MN26.html",
-    category: "Core Teachings",
+    title: "Goodwill (2) / Mettā Sutta",
+    content: `“Monks, there are these four types of individuals to be found existing in the world. Which four?
+              “There is the case where an individual keeps pervading the first direction [the east], as well as the second direction, the third, & the fourth, with an awareness imbued with goodwill. Thus he keeps pervading above, below, & all around, everywhere & in every respect the all-encompassing cosmos with an awareness imbued with goodwill: abundant, expansive, immeasurable, free from hostility, free from ill will. He regards whatever phenomena there that are connected with form, feeling, perception, fabrications, & consciousness, as inconstant, stressful, a disease, a cancer, an arrow, painful, an affliction, alien, a disintegration, an emptiness, not-self. At the break-up of the body, after death, he reappears in conjunction with the Devas of the Pure Abodes. This rebirth is not in common with run-of-the-mill people.
+              “Again, there is the case where an individual keeps pervading the first direction [the east], as well as the second direction, the third, & the fourth, with an awareness imbued with compassion. Thus he keeps pervading above, below, & all around, everywhere & in every respect the all-encompassing cosmos with an awareness imbued with compassion: abundant, expansive, immeasurable, free from hostility, free from ill will. He regards whatever phenomena there that are connected with form, feeling, perception, fabrications, & consciousness, as inconstant, stressful, a disease, a cancer, an arrow, painful, an affliction, alien, a disintegration, an emptiness, not-self. At the break-up of the body, after death, he reappears in conjunction with the Devas of the Pure Abodes. This rebirth is not in common with run-of-the-mill people.
+              “Again, there is the case where an individual keeps pervading the first direction [the east], as well as the second direction, the third, & the fourth, with an awareness imbued with empathetic joy. Thus he keeps pervading above, below, & all around, everywhere & in every respect the all-encompassing cosmos with an awareness imbued with empathetic joy: abundant, expansive, immeasurable, free from hostility, free from ill will. He regards whatever phenomena there that are connected with form, feeling, perception, fabrications, & consciousness, as inconstant, stressful, a disease, a cancer, an arrow, painful, an affliction, alien, a disintegration, an emptiness, not-self. At the break-up of the body, after death, he reappears in conjunction with the Devas of the Pure Abodes. This rebirth is not in common with run-of-the-mill people.
+              “Again, there is the case where an individual keeps pervading the first direction [the east], as well as the second direction, the third, & the fourth, with an awareness imbued with equanimity. Thus he keeps pervading above, below, & all around, everywhere & in every respect the all-encompassing cosmos with an awareness imbued with equanimity: abundant, expansive, immeasurable, free from hostility, free from ill will. He regards whatever phenomena there that are connected with form, feeling, perception, fabrications, & consciousness, as inconstant, stressful, a disease, a cancer, an arrow, painful, an affliction, alien, a disintegration, an emptiness, not-self. At the break-up of the body, after death, he reappears in conjunction with the Devas of the Pure Abodes. This rebirth is not in common with run-of-the-mill people.
+              “These are four types of individuals to be found existing in the world.”`,
+    source: "AN 4:126",
+    sourceUrl: "https://www.dhammatalks.org/suttas/AN/AN4_126.html",
+    category: "Goodwill",
   },
   {
     title: "The Ending of Craving",
